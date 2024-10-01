769865b709a8711ab0ace7d022d891c2384be210
rodri <rodriirage@gmail.com>
2024-10-01 18:08:48 -0300

    Preentrega04

--- js/main.js
+++ js/main.js
@@ -3,16 +3,18 @@ let notesArray = [];
 const addNote = document.querySelector('#addNote');
 const addForm = document.querySelector('#addForm');
 const addInput = document.querySelector('#addInput');
+const titleInput = document.querySelector('#titleInput');
 const saveNote = document.querySelector('#addBtn');
 const changeColor = document.querySelector('#changeColorBg');
 
 function displayNotes() {
     addNote.innerHTML = '';
 
-    notesArray.forEach((element, index) => {
+    notesArray.forEach((note, index) => {
         let noteHTML = `
                 <div class="nota1">
-                    <p>${element}</p>
+                    <h3>${note.title}</h3>
+                    <p>${note.content}</p>
                     <p onclick="deleteNote(${index})" class="btnEliminar"><i class="bx bx-trash"></i></p>
                 </div>`;
         addNote.innerHTML += noteHTML;
@@ -25,16 +27,48 @@ addForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     if (addInput.value.trim() !== '') {
-        notesArray.push(addInput.value);
-        addInput.focus();
+        const newNote = {
+            title: titleInput.value,
+            content: addInput.value
+        };
+
+        Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: '¡Nota guardada!',
+            showConfirmButton: false,
+            timer: 900
+        })
+
+        notesArray.push(newNote);
+        titleInput.focus();
         addForm.reset();
         displayNotes();
     }
 });
 
 function deleteNote(index) {
-    notesArray.splice(index, 1);
-    displayNotes();
+    Swal.fire({
+        title: '¿Estás seguro?',
+        text: 'Esta acción eliminará la nota de forma permanente.',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sí, eliminar',
+        cancelButtonText: 'Cancelar'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            notesArray.splice(index, 1);
+            displayNotes();
+
+            Swal.fire(
+                'Eliminada',
+                'La nota ha sido eliminada correctamente.',
+                'success'
+            )
+        }
+    });
 }
 
 function loadNotes() {
@@ -68,4 +102,4 @@ document.querySelector('#changeColorBg').addEventListener('click', () => {
         });
 });
 
-document.addEventListener('DOMContentLoaded', loadNotes);   
+document.addEventListener('DOMContentLoaded', loadNotes);
